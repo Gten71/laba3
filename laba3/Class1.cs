3977c574b57da4e5a13a6fc43aa099f2b60367b7
Gten <mresnyansky@gmail.com>
2022-06-12 11:14:29 +0200

    Добавьте файлы проекта.

---
+++ laba3/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba3
+{
+    internal class Raz
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public Raz(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        public double Chislo()
+        {
+            return (A-B/2)*C;
+        }
+    }
+}
---
+++ laba3/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laba3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Solve_Click(object sender, RoutedEventArgs e)
+        {
+            Raz raz = new Raz(double.Parse(First.Text), double.Parse(Second.Text), double.Parse(Third.Text));
+            Result.Text = raz.Chislo().ToString();
+        }
+    }
+}
